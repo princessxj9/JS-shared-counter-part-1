7bd88fd27663c3a55a9e53109ad8dfde937c2f91
princessxj9 <johnsonprincess25@gmail.com>
2022-04-23 11:40:46 -0400

    commit

--- public/index.js
+++ public/index.js
@@ -2,17 +2,38 @@ function main(){
     const countContainer = document.querySelector('#count-container');
     const incrementButton = document.querySelector('#increment-button');
     const decrementButton = document.querySelector('#decrement-button');
+     
+    const response = await fetch('http://localhost:9001/counter');
+    const results = await response.json();
 
-    let countValue = 0;
+    const countValue = results.value;
 
-    function increment(){
+async function increment(){
         countValue++;
         countContainer.textContent = countValue;
-    }
+ await fetch('http://localhost:9001/counter',{
+    method: 'PATCH', 
+    headers: {
+        'Content-Type': 'application/json'},
+        body: JSON.stringify({
+        value: countValue 
+       
+        })
+    })
+}
 
-    function decrement(){
+   async function decrement(){
         countValue--;
         countContainer.textContent = countValue;
+  await fetch('http://localhost:9001/counter',{
+    method: 'PATCH', 
+    headers: {
+        'Content-Type': 'application/json'},
+        body: JSON.stringify({
+        value: countValue 
+       
+        })
+    })
     }
 
     incrementButton.addEventListener('click', increment);
